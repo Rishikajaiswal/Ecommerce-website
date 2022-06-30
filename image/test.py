b222aca6b4db5617680a9e81c34ebbd9c0031e0e
Rishikajaiswal <71859732+Rishikajaiswal@users.noreply.github.com>
2022-06-30 08:00:17 +0530

    Add files via upload

---
+++ image/test.py
@@ -0,0 +1,12 @@
+num = input("enter the numerator digits")
+num2 =input("Enter the denominator digits")
+
+for m in range(1,5):
+    if num2==0:
+      raise Exception:
+    print("Number cannot be divided by 0")
+    else:
+        {
+            print(num/num2)
+        }
+   
